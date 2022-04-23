dcba18a4ffc9ff7af0896b4460c8d3948864267b
jnino129 <104236235+jnino129@users.noreply.github.com>
2022-04-22 19:53:22 -0500

    librerias

--- Modulo 1 - Cadena de Bloques/Blockchain.py
+++ Modulo 1 - Cadena de Bloques/Blockchain.py
@@ -6,3 +6,14 @@ Created on Fri Apr 22 18:54:32 2022
 """
 
 #Modulo 1 - construcion de la cadena de bloques 
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = "0")
+        
